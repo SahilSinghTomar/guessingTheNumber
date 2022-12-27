19f867f7710550452749f969bbe083c9faebe1cd
SahilSinghTomar <50849361+SahilSinghTomar@users.noreply.github.com>
2022-12-27 23:02:21 +0530

    First Commit

---
+++ script.js
@@ -0,0 +1,52 @@
+'use strict';
+
+function gameStatus(status, score) {
+  if (score > 1) {
+    document.querySelector('.message').textContent = `Too ${status}!👆`;
+    score--;
+    document.querySelector('.score').textContent = score;
+  } else {
+    document.querySelector('.message').textContent = 'You lost the game💥';
+    score = 0;
+    document.querySelector('.score').textContent = score;
+  }
+}
+
+let number = Math.trunc(Math.random() * 20) + 1;
+// document.querySelector('.number').textContent = number;
+
+let score = 20;
+let highscore = 0;
+
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+
+  if (!guess) {
+    document.querySelector('.message').textContent = 'No Value Entered🚫';
+  } else if (guess === number) {
+    document.querySelector('.message').textContent = 'Correct Number🎉';
+    document.querySelector('.number').textContent = number;
+    document.querySelector('body').style.backgroundColor = '#60b347';
+    document.querySelector('.number').style.width = '30rem';
+
+    if (highscore < score) {
+      highscore = score;
+      document.querySelector('.highscore').textContent = highscore;
+    }
+  } else if (guess > number) {
+    gameStatus('High', score);
+  } else {
+    gameStatus('Low', score);
+  }
+});
+
+document.querySelector('.again').addEventListener('click', function () {
+  number = Math.trunc(Math.random() * 20) + 1;
+  document.querySelector('body').style.backgroundColor = '#222';
+  document.querySelector('.message').textContent = 'Start guessing...';
+  document.querySelector('.number').textContent = '?';
+  document.querySelector('.number').style.width = '15rem';
+  score = 20;
+  document.querySelector('.score').textContent = score;
+  document.querySelector('.guess').value = '';
+});
